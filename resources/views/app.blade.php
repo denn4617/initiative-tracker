2a0e8767144705aee3b3ff56208f4ad3e9fb6a84
denn4617 <dkilia19@student.aau.dk>
2025-08-24 23:31:44 +0200

    update styling + naming

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Initiative Tracker</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+</head>
+
+<body>
+    <div id="app"></div>
+</body>
+
+</html>
